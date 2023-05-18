e0ec0457d05cee94d6d6ae0fec32e5c34ea4e18e
AntonBd <a.budanov@ngs.ru>
2023-05-18 17:38:55 +0300

    Ready tasks 1, 2, 3, 11
    Ready:
    - Task 1: Investigate project structure;
    - Task 2: Delete social networks: Yandex, VK;
    - Task 3: Take out sensitive information to property file;
    - Task 11: Localize main page and mail patterns to two languages

---
+++ src/main/java/com/javarush/jira/common/language/MessageConfig.java
@@ -0,0 +1,29 @@
+package com.javarush.jira.common.language;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+@Configuration
+public class MessageConfig implements WebMvcConfigurer {
+
+    @Bean
+    public SessionLocaleResolver localeResolver() {
+        return new SessionLocaleResolver();
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor interceptor = new LocaleChangeInterceptor();
+        interceptor.setParamName("lang");
+        return interceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+}
